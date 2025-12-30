16241077db49b21ccfafec18797147cd63768687
ssanchezguillan <202401251@alu.comillas.edu>
2025-12-30 14:14:06 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -20,12 +20,23 @@ def load_data()->pd.DataFrame:
     url1 = "https://github.com/ssanchezguillan/practica_final_streamlit/releases/download/v1.0/parte_1.csv"
     url2 = "https://github.com/ssanchezguillan/practica_final_streamlit/releases/download/v1.0/parte_2.csv"
 
-    df1 = pd.read_csv(url1)
-    df2 = pd.read_csv(url2)
+    usecols = [
+        "date", "store_nbr", "family", "sales", "onpromotion",
+        "transactions", "city", "state", "type"
+    ]
+
+    df1 = pd.read_csv(url1, usecols=usecols)
+    df2 = pd.read_csv(url2, usecols=usecols)
 
     df = pd.concat([df1,df2], ignore_index=True)
 
-    df["date"] = pd.to_datetime(df["date"]) #convertimos la columna date a tipo datetime
+    df["date"] = pd.to_datetime(df["date"], errors="coerce") #convertimos la columna date a tipo datetime
+
+    df["day_of_week"] = df["date"].dt.day_name()
+    df["month"] = df["date"].dt.month
+    df["day"] = df["date"].dt.day
+    df["year"] = df["date"].dt.year
+    df["week"] = df["date"].dt.isocalendar().week.astype(int)
 
     return df
 
